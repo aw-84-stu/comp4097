59345a2938679271141d62ec1930ec0ed5e0c6f6
aw-84-stu <21211043@life.hkbu.edu.hk>
2022-11-30 06:40:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/firebase/HomeActivity.kt
@@ -0,0 +1,25 @@
+package com.example.firebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+
+class HomeActivity : AppCompatActivity() {
+    private  lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        auth = FirebaseAuth.getInstance()
+        val email = intent.getStringExtra("email")
+        val name = intent.getStringExtra("name")
+        findViewById<TextView>(R.id.textView).text = email + "\n" + name
+        findViewById<Button>(R.id.signOutBtn).setOnClickListener(){
+        auth.signOut()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
